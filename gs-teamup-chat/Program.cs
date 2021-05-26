2a42d0946b54152b3391719e1ad2a3c0b1f60126
sonhyungwoo <dennis91@estsoft.com>
2021-05-26 14:46:58 +0900

    인증 실패시 처리 수정

--- gs-teamup-chat/Program.cs
+++ gs-teamup-chat/Program.cs
@@ -20,14 +20,18 @@ namespace GsTeamupChat
                 if (eventInfo == null)
                 {
                     Console.WriteLine("ev 정보를 받아올 수 없습니다.");
-                    System.Threading.Thread.Sleep(1000 * 5);
-                    continue;
+                    System.Threading.Thread.Sleep(1000 * 3);
+                    return;
                 }
                 for (int i = 0; i < 86400 / (eventInfo.lpWaitTimeout + 5) + 1; i++)
                 {
                     TeamupEventList eventWrap = eventService.Polling((eventInfo.lpWaitTimeout + 5) * 1000);
-
-                    if (eventWrap != null && eventWrap.events != null && eventWrap.events.Length != 0)
+                    if(eventWrap == null)
+                    {
+                        System.Threading.Thread.Sleep(1000 * 3);
+                        return;
+                    }
+                    if (eventWrap.events != null && eventWrap.events.Length != 0)
                     {
                         for (int j = 0; j < eventWrap.events.Length; j++)
                         {
